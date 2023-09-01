0345797ecb439bf45c982be0faedee06ddf5e762
Stephen Muema <105632674+Stephen11221@users.noreply.github.com>
2023-09-01 19:59:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+let boxName = "Welcome to"
+let Boxname = "Design House"
+
+document.getElementById("upper").innerHTML = boxName;
+document.getElementById("lower").innerHTML = Boxname;
+let companylogo = "Design";
+let companylogo$ = "House";
+
+
+document.getElementById("cnm1").innerHTML = companylogo;
+document.getElementById("cnm2").innerHTML = companylogo$
+
+let developer = "Designed by Junior Softwere Engineer Stephen Muema";
+document.getElementById("develop-std").innerHTML = developer;
+
+
+
+document.getElementById('get-started').onclick = function () {
+    let = window.alert('Design house')
+}
